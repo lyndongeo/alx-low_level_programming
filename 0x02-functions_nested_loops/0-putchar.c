d5c96bcb7721a69d510f278580b8abd3eda8d0c9
lyndongeo <lyndongeorge7@gmail.com>
2022-07-06 03:04:28 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * main - Entry point
  * Description: Program that prints putchar followed by a new line
@@ -8,15 +9,15 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	return (0);
+return (0);
 }
