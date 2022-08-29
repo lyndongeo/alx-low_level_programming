0477ca569e83aa4f72185fe9ce94311e3d87b691
lyndongeo <lyndongeorge7@gmail.com>
2022-08-29 12:24:20 -0700

    Task 6

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -10,5 +10,10 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
+int countWords(char *s);
+int endIndex(char *s, int index);
+int startIndex(char *s, int index);
+int isSpace(char c);
 
 #endif
