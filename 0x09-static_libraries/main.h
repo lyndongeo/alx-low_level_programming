eeedb1645bdd679f7bec3a0515c6c6cc6f2dd2ef
lyndongeo <lyndongeorge7@gmail.com>
2023-07-07 06:00:35 -0700

    commit

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -8,8 +8,10 @@ int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
+int _isalpha(int c);
 int _isdigit(int c);
 int _strlen(char *s);
+char *_strpbrk(char *s, char *accept);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
@@ -21,7 +23,6 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 #endif
